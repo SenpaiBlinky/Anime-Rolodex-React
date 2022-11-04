7c96b5b522ae2daf0931b38b91bab76740a2bcff
Master <blinkerion@gmail.com>
2022-11-04 16:35:44 -0500

    rendering api list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css"
 
 function App() {
+
+
+
+    let [animeList, updateAnime] = useState([])
+
+    useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => updateAnime(() => users),
+     () => console.log(animeList))
+    })
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {animeList.map(anime => <h1 key={anime.id}>{anime.name}</h1>)}
+
     </div>
   );
 }
